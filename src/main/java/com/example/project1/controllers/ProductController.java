069793336872a50228d75dc21563199051c11d06
washington-shoji <shoji.washington@gmail.com>
2023-11-24 10:54:47 +1100

    -updated product controller mapping entity to dto

--- src/main/java/com/example/project1/controllers/ProductController.java
+++ src/main/java/com/example/project1/controllers/ProductController.java
@@ -1,7 +1,10 @@
 package com.example.project1.controllers;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,41 +15,66 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.project1.dto.ProductDto;
 import com.example.project1.entities.Product;
 import com.example.project1.services.impl.ProductServiceImpl;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
+
     private final ProductServiceImpl productService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public ProductController(ProductServiceImpl productService) {
+    public ProductController(ProductServiceImpl productService, ModelMapper modelMapper) {
         this.productService = productService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
-    List<Product> getAllProductList() {
-        return productService.findAll();
+    List<ProductDto> getAllProductList() {
+        List<Product> products = productService.findAll();
+        return products.stream().map(this::convertToDto).collect(Collectors.toList());
     }
 
     @GetMapping("/by-id")
-    Product getProductById(@RequestParam(value = "id") Long id) {
-        return productService.findById(id);
+    ProductDto getProductById(@RequestParam(value = "id") Long id) {
+        Product product = productService.findById(id);
+        return convertToDto(product);
     }
 
     @PostMapping("/create")
-    Product createProduct(@RequestBody Product product) {
-        return productService.saveProduct(product);
+    ProductDto createProduct(@RequestBody ProductDto productDto) {
+
+        Product product = converteToEntity(productDto);
+        Product productCreated = productService.saveProduct(product);
+        return convertToDto(productCreated);
     }
 
     @PutMapping("/update")
-    Product updateProduct(@RequestParam(value = "id") Long id, @RequestBody Product product) {
-        return productService.updateProduct(id, product);
+    ProductDto updateProduct(@RequestParam(value = "id") Long id, @RequestBody ProductDto productDto) {
+        if (!Objects.equals(id, productDto.getId())) {
+            throw new IllegalArgumentException("Ids don't match");
+        }
+        Product product = converteToEntity(productDto);
+        Product updatedProduct = productService.updateProduct(id, product);
+        return convertToDto(updatedProduct);
     }
 
     @DeleteMapping("/delete")
     void deleteProduct(@RequestParam(value = "id") Long id) {
         productService.deleteProduct(id);
     }
+
+    private ProductDto convertToDto(Product product) {
+        ProductDto productRequestDto = modelMapper.map(product, ProductDto.class);
+        return productRequestDto;
+    }
+
+    private Product converteToEntity(ProductDto productDto) {
+        Product product = modelMapper.map(productDto, Product.class);
+        return product;
+    }
+
 }
