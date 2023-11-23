b6e546dc052460a5cd59aac5c7933c004e3f7d60
washington-shoji <shoji.washington@gmail.com>
2023-11-24 10:53:12 +1100

    -added productDto

---
+++ src/main/java/com/example/project1/dto/ProductDto.java
@@ -0,0 +1,50 @@
+package com.example.project1.dto;
+
+public class ProductDto {
+    private Long id;
+    private String productName;
+    private String productDescription;
+    private String productPrice;
+    private String productImage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+}
