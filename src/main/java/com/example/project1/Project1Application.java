8ca5f3928a04607fd3d0b7a7c7c47690cb86fdf3
washington-shoji <washington.shoji@automic.com.au>
2023-11-23 21:45:19 +1100

    -added cors initil loose config

--- src/main/java/com/example/project1/Project1Application.java
+++ src/main/java/com/example/project1/Project1Application.java
@@ -2,12 +2,28 @@ package com.example.project1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Project1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsMappingConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200/")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
