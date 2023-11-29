9b4d1bfe6e5746393e5727d60d7aa566aa132d11
washington-shoji <shoji.washington@gmail.com>
2023-11-29 15:47:22 +1100

    -added an event dto

---
+++ src/main/java/com/example/project1/dto/EventDto.java
@@ -0,0 +1,31 @@
+package com.example.project1.dto;
+
+public class EventDto {
+    private Long id;
+    private String title;
+    private String shortDescription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+}
