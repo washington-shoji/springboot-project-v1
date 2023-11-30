2565d92169d66ab13fa748de078f5baef13aa337
washington-shoji <shoji.washington@gmail.com>
2023-11-30 16:46:56 +1100

    -added event controller

---
+++ src/main/java/com/example/project1/controllers/EventController.java
@@ -0,0 +1,10 @@
+package com.example.project1.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("event")
+public class EventController {
+
+}
