8ecd74722fb870672dae64dcbb4665a7e1ea4cbd
washington-shoji <shoji.washington@gmail.com>
2023-12-04 09:13:22 +1100

    -updated event repository to find event by uuid

--- src/main/java/com/example/project1/repositories/EventRepository.java
+++ src/main/java/com/example/project1/repositories/EventRepository.java
@@ -1,11 +1,16 @@
 package com.example.project1.repositories;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.project1.entities.Event;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, UUID> {
     boolean existsByTitle(String title);
+
+    Optional<Event> findById(UUID id);
 }
