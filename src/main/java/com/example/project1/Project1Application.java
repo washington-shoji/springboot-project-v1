57bb8c0638d2f7539c39ad49a9fd21664d7477ff
washington-shoji <shoji.washington@gmail.com>
2023-12-06 11:20:36 +1100

    -added app level cloudinary beam

--- src/main/java/com/example/project1/Project1Application.java
+++ src/main/java/com/example/project1/Project1Application.java
@@ -4,8 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.cloudinary.Cloudinary;
 
 @SpringBootApplication
 public class Project1Application {
@@ -30,6 +33,11 @@ public class Project1Application {
 	@Bean
 	ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	Cloudinary cloudinary(Environment environment) {
+		return new Cloudinary(environment.getProperty("application.cloudinary.cloudinary-url"));
 	}
 
 }
