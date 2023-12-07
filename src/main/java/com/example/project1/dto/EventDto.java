59a372c3365a11f98fe075c56a10765305db6726
washington-shoji <shoji.washington@gmail.com>
2023-12-07 16:35:12 +1100

    -updated event dto

--- src/main/java/com/example/project1/dto/EventDto.java
+++ src/main/java/com/example/project1/dto/EventDto.java
@@ -1,14 +1,14 @@
 package com.example.project1.dto;
 
-import java.util.Date;
 import java.util.UUID;
 
 public class EventDto {
     private UUID id;
     private String title;
     private String shortDescription;
-    private Date eventDate;
-    private Date eventRegistrationCloseDate;
+    private String eventImage;
+    private String eventDate;
+    private String eventRegistrationCloseDate;
 
     public UUID getId() {
         return id;
@@ -34,20 +34,35 @@ public class EventDto {
         this.shortDescription = shortDescription;
     }
 
-    public Date getEventDate() {
+    public String getEventDate() {
         return eventDate;
     }
 
-    public void setEventDate(Date eventDate) {
+    public void setEventDate(String eventDate) {
         this.eventDate = eventDate;
     }
 
-    public Date getEventRegistrationCloseDate() {
+    public String getEventRegistrationCloseDate() {
         return eventRegistrationCloseDate;
     }
 
-    public void setEventRegistrationCloseDate(Date eventRegistrationCloseDate) {
+    public void setEventRegistrationCloseDate(String eventRegistrationCloseDate) {
         this.eventRegistrationCloseDate = eventRegistrationCloseDate;
+    }
+
+    public String getEventImage() {
+        return eventImage;
+    }
+
+    public void setEventImage(String eventImage) {
+        this.eventImage = eventImage;
+    }
+
+    @Override
+    public String toString() {
+        return "EventDto [id=" + id + ", title=" + title + ", shortDescription=" + shortDescription + ", eventImage="
+                + eventImage + ", eventDate=" + eventDate + ", eventRegistrationCloseDate=" + eventRegistrationCloseDate
+                + "]";
     }
 
 }
