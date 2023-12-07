f0a4d9cffae064f47d2fcb378c2d2cd96745eeda
washington-shoji <shoji.washington@gmail.com>
2023-12-07 16:36:56 +1100

    -updated event controller

--- src/main/java/com/example/project1/controllers/EventController.java
+++ src/main/java/com/example/project1/controllers/EventController.java
@@ -2,18 +2,20 @@ package com.example.project1.controllers;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.project1.dto.EventDto;
 import com.example.project1.entities.Event;
@@ -27,7 +29,9 @@ public class EventController {
     private final ModelMapper modelMapper;
 
     @Autowired
-    public EventController(EventServiceImpl eventService, ModelMapper modelMapper) {
+    public EventController(
+            EventServiceImpl eventService,
+            ModelMapper modelMapper) {
         this.eventService = eventService;
         this.modelMapper = modelMapper;
     }
@@ -39,30 +43,32 @@ public class EventController {
     }
 
     @GetMapping("/by-id")
-    EventDto getEventById(@RequestParam(value = "id") Long id) {
+    EventDto getEventById(@RequestParam(value = "id") UUID id) {
         Event event = eventService.findById(id);
         return convertToDto(event);
     }
 
     @PostMapping("/create")
-    EventDto createEvent(@RequestBody EventDto eventDto) {
+    EventDto createEvent(@ModelAttribute EventDto eventDto,
+            @RequestParam("image") MultipartFile multipartFile) {
         Event event = converteToEntity(eventDto);
-        Event eventCreated = eventService.saveEvent(event);
+        Event eventCreated = eventService.saveEvent(event, multipartFile);
         return convertToDto(eventCreated);
     }
 
     @PutMapping("/update")
-    EventDto upddateEvent(@RequestParam(value = "id") Long id, @RequestBody EventDto eventDto) {
+    EventDto upddateEvent(@RequestParam(value = "id") UUID id, @ModelAttribute EventDto eventDto,
+            @RequestParam("image") MultipartFile multipartFile) {
         if (!Objects.equals(id, eventDto.getId())) {
             throw new IllegalArgumentException("Ids don't match");
         }
         Event event = converteToEntity(eventDto);
-        Event updatedEvent = eventService.updateEvent(id, event);
+        Event updatedEvent = eventService.updateEvent(id, event, multipartFile);
         return convertToDto(updatedEvent);
     }
 
     @DeleteMapping("/delete")
-    void deleteEvent(@RequestParam(value = "id") Long id) {
+    void deleteEvent(@RequestParam(value = "id") UUID id) {
         eventService.deleteEvent(id);
     }
 
