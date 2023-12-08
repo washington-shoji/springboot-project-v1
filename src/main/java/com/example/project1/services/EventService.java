90c8acc83d1f800edcf6989fb2f6aa38cb00f926
washington-shoji <shoji.washington@gmail.com>
2023-12-08 17:15:06 +1100

    -updated event service

--- src/main/java/com/example/project1/services/EventService.java
+++ src/main/java/com/example/project1/services/EventService.java
@@ -3,6 +3,8 @@ package com.example.project1.services;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.example.project1.entities.Event;
 
 public interface EventService {
@@ -10,9 +12,9 @@ public interface EventService {
 
     Event findById(UUID id);
 
-    Event saveEvent(Event event);
+    Event saveEvent(Event event, MultipartFile multipartFile);
 
-    Event updateEvent(UUID id, Event event);
+    Event updateEvent(UUID id, Event event, MultipartFile multipartFile);
 
     void deleteEvent(UUID id);
 }
